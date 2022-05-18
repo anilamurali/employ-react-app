321cd453cf1201bfde04d6b140489a61f3552791
anilamurali <anilamurali98@gmail.com>
2022-05-18 21:06:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addemploy from './Component/Addemploy';
+import Searchemploye from './Component/Searchemploye';
 
 function App() {
   return (
     
     <>
-    <Addemploy/>
+    <Searchemploye/>
     </>
   );
 }
